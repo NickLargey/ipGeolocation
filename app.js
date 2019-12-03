2872a0c3848d89e9356c171ec8a6ad7548617616
Nick Largey <Nick.Largey@gmail.com>
2019-12-03 00:20:43 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const submitBtn = document.getElementById("ipSubmit");
-const maps ="https://www.google.com/maps/embed/v1/place?key=AIzaSyCahhqGFfzuUx1UW-tUfJR7gj2R3iFr0-0&q="
-const ipAPI = "http://ip-api.com/json/"
+const maps ="https://www.google.com/maps/embed/v1/place?key=AIzaSyCahhqGFfzuUx1UW-tUfJR7gj2R3iFr0-0&q=";
 
 window.onload = function getIpInfo(){
   let resolveIP = localStorage.getItem('ip');
@@ -10,7 +9,9 @@ window.onload = function getIpInfo(){
   .then((data) => {
     let output = '<h1 class="display-2 mb-4 text-white text-center">IP Info<row class="d-flex"></row></h1>';
     Object.keys(data).forEach(function(key) {
-      output += `<div class="d-inline-flex p-2 m-2 text-white border border-black">${key} : ${data[key]}</div>`;
+      output += `
+        <div class="d-inline-flex p-2 m-2 text-white border border-black">${key} : ${data[key]}</div>
+        `;
     });
 
     let iframe = document.createElement('iframe');
